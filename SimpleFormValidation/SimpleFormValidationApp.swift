517b39642e8db6ae38ff8951eee7c540bf5954e0
DavidAdewale <adewaledavidola@gmail.com>
2024-07-11 13:15:46 +0100

    Initial Commit

---
+++ SimpleFormValidation/SimpleFormValidationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleFormValidationApp.swift
+//  SimpleFormValidation
+//
+//  Created by David Adewale on 11/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct SimpleFormValidationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
